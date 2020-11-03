7167ed5e37ac21ce1cc766e1924860dace52e474
Patrick Lake <plake492@gmail.com>
2020-11-02 22:47:55 -0500

    Update end game msg // Add restart feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const box = document.querySelectorAll('.box')
 let userOne = true
 let moves = 0
 
-const gameArr = [
+let gameArr = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
@@ -20,13 +20,13 @@ const game = (box) => {
     return
   }
   if (userOne) box.textContent = 'O'
-  if (!userOne)box.textContent = 'X'
-  userOne = !userOne
+  if (!userOne) box.textContent = 'X'
+
   moves++
   if (moves === 9) {
-    endGame.textContent = 'Game Over'
     gameOver()
   }
+
   logic(box)
 }
 
@@ -36,41 +36,66 @@ const logic = (box) => {
   gameArr[colI][rowI] = box.textContent
 
   if (gameArr[0][0] === gameArr[0][1] && gameArr[0][1] === gameArr[0][2]) {
-    endGame.textContent = gameArr[0][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[1][0] === gameArr[1][1] && gameArr[1][1] === gameArr[1][2]) {
-    endGame.textContent = gameArr[1][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[2][0] === gameArr[2][1] && gameArr[2][1] === gameArr[2][2]) {
-    endGame.textContent = gameArr[2][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[0][0] === gameArr[1][0] && gameArr[1][0] === gameArr[2][0]) {
-    endGame.textContent = gameArr[0][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[0][1] === gameArr[1][1] && gameArr[1][1] === gameArr[2][1]) {
-    endGame.textContent = gameArr[0][1] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[0][2] === gameArr[1][2] && gameArr[1][2] === gameArr[2][2]) {
-    endGame.textContent = gameArr[0][2] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[0][0] === gameArr[1][1] && gameArr[1][1] === gameArr[2][2]) {
-    endGame.textContent = gameArr[0][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
   if (gameArr[0][2] === gameArr[1][1] && gameArr[1][1] === gameArr[2][0]) {
-    endGame.textContent = gameArr[0][0] + ' wins'
-    gameOver()
+    return gameOver()
   }
+  userOne = !userOne
 }
 
 const gameOver = () => {
   box.forEach(item => {
     item.setAttribute('style', 'pointer-events:none')
   })
+  let endText
+  if (moves === 9) { endText = "It's a Draw" } else {
+    if (userOne) { endText = 'O WINS!' }
+    if (!userOne) { endText = 'X WINS!' }
+  }
+  endGame.setAttribute('style', 'display: block')
+  endGame.textContent = endText
+  const btnEl = document.createElement('button')
+  btnEl.textContent = 'Play Again'
+  btnEl.id = 'restart'
+  endGame.appendChild(btnEl)
 }
+
+const restartGame = () => {
+  userOne = true
+  moves = 0
+  gameArr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  endGame.setAttribute('style', 'display: none')
+  box.forEach(item => {
+    item.textContent = ''
+    item.setAttribute('style', 'pointer-events:initial')
+  })
+}
+
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'restart') {
+    restartGame()
+  }
+})
